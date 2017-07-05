56b09b2e214b5610a7fb8916cfd82bfe12613d06
ansen dong <ddttcc@gmail.com>
2017-07-05 12:29:27 +0800

    syscall gettid

---
+++ process/getthreadid.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+     pid_t tid;
+
+     tid = syscall(SYS_gettid);
+     printf("tid : %d\n",tid);
+     
+     sleep(50);
+     //tid = syscall(SYS_tgkill, getpid(), tid, SIGHUP);
+     //printf("tid : %d ...\n",tid);
+}
+
+
