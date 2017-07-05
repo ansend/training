75283dc539e3c01f2e21fc9921b884f218aae5e3
ansen dong <ddttcc@gmail.com>
2017-07-05 13:39:08 +0800

    add a test to get thread id of child thread

--- thread/pthread_once.c
+++ thread/pthread_once.c
@@ -5,7 +5,25 @@
 
 #include <stdio.h>
 #include <pthread.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+
 pthread_once_t  once=PTHREAD_ONCE_INIT;
+
+// actually, in linux one thread is a LWP, so we can using a system call to get the pid of the LWP.
+int get_thread_id()
+{
+     pid_t tid;
+     tid = syscall(SYS_gettid);
+     printf("pid of LWP in thread context, tid : %d\n",tid);
+     return tid;
+
+}
+
 void    once_run(void)
 {
         printf("once_run in thread %d\n",pthread_self());
@@ -14,6 +32,7 @@ void * child1(void *arg)
 {
         int tid=pthread_self();
         printf("thread %d enter\n",tid);
+        get_thread_id();
         pthread_once(&once,once_run);
         printf("thread %d returns\n",tid);
 }
@@ -21,16 +40,17 @@ void * child2(void *arg)
 {
         int tid=pthread_self();
         printf("thread %d enter\n",tid);
+        get_thread_id();
         pthread_once(&once,once_run);
         printf("thread %d returns\n",tid);
 }
 int main(void)
 {
-        int tid1,tid2;
+        pthread_t tid1,tid2;
         printf("hello\n");
         pthread_create(&tid1,NULL,child1,NULL);
         pthread_create(&tid2,NULL,child2,NULL);
-        sleep(10);
+        sleep(20);
         printf("main thread exit\n");
         return 0;
 }
