77ac8e5decc5611e9f31729b37550e7d2a78791c
ansen dong <ddttcc@gmail.com>
2018-04-25 11:54:51 +0800

    forward declare

---
+++ c++/forward_declare/A.h
@@ -0,0 +1,14 @@
+#ifndef A_INCLUDE_H
+#define A_INCLUDE_H
+
+#include "B.h"
+class A
+{
+    int a;
+    B   b;
+};
+
+
+#else
+#endif
+
---
+++ c++/forward_declare/main.cpp
@@ -0,0 +1,7 @@
+#include "A.h"
+#include "B.h"
+int main(int argc, char ** argv)
+{
+    A a;
+
+}
